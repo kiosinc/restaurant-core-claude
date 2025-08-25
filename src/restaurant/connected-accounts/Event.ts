251e43aba3286634f2d4c8c1c099a2611170f367
jjc235 <jjc@onlumina.com>
2025-08-24 17:01:12 -0700

    Event count cap

--- src/restaurant/connected-accounts/Event.ts
+++ src/restaurant/connected-accounts/Event.ts
@@ -12,6 +12,7 @@ export default class Event extends FirestoreObject {
   isSync: boolean;
 
   queueCap: number;
+  queueCount: number;
 
   timestamp?: Date;
 
@@ -20,6 +21,7 @@ export default class Event extends FirestoreObject {
     type: string,
     isSync: boolean,
     queueCap?: number,
+    queueCount?: number,
     timestamp?: Date,
     created?: Date,
     updated?: Date,
@@ -30,7 +32,19 @@ export default class Event extends FirestoreObject {
     this.provider = provider;
     this.type = type;
     this.isSync = isSync;
-    this.queueCap = queueCap ?? -1;
+
+    if (queueCap === undefined) {
+      this.queueCap = -1;
+    } else {
+      this.queueCap = queueCap;
+    }
+
+    if (queueCount === undefined) {
+      this.queueCount = 0;
+    } else {
+      this.queueCount = queueCount;
+    }
+
     this.timestamp = timestamp;
   }
 
@@ -81,6 +95,7 @@ export default class Event extends FirestoreObject {
         type: event.type,
         isSync: event.isSync,
         queueCap: event.queueCap,
+        queueCount: event.queueCount,
         timestamp: event.timestamp?.toISOString() ?? '',
         created: event.created.toISOString(),
         updated: event.updated.toISOString(),
@@ -90,11 +105,20 @@ export default class Event extends FirestoreObject {
     fromFirestore(snapshot: FirebaseFirestore.QueryDocumentSnapshot): Event {
       const data = snapshot.data();
 
+      let queueCap = data.queueCap
+      if (queueCap === undefined) {
+        queueCap = -1;
+      }
+      let queueCount = data.queueCount;
+      if (queueCount === undefined) {
+        queueCount = 0;
+      }
       return new Event(
         data.provider,
         data.type,
         data.isSync,
-        data.queueCap ?? -1,
+        queueCap,
+        queueCount,
         data.timestamp === '' ? undefined : new Date(data.timestamp),
         new Date(data.created),
         new Date(data.updated),
